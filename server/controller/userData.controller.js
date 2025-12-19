77ccec3f24f9a679c2e6d0fbc9672fe4e0102b4d
Shahzaib <sjobs5245@gmail.com>
2025-12-19 19:41:28 +0500

    first commit

--- server/controller/userData.controller.js
+++ server/controller/userData.controller.js
@@ -1,5 +1,7 @@
 import userModel from "../model/userData.model.js";
 import bcrypt from "bcrypt";
+import CryptoJS from "crypto-js";
+import { v4 as uuidv4 } from "uuid";
 
 /* ================= REGISTER ================= */
 export const doRegister = async (req, res) => {
@@ -37,6 +39,8 @@ export const doRegister = async (req, res) => {
 };
 
 /* ================= LOGIN ================= */
+// temporary data Storage   sub users ki ids save kerne k liye
+const sessionKeys = {};
 export const doLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -49,13 +53,19 @@ export const doLogin = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ ok: false, message: "Invalid password" });
-    }
+    } else {
+      const dynamicKey = CryptoJS.lib.WordArray.random(16).toString();
+      const sessionID = uuidv4();
+      const encryptedMail = CryptoJS.AES.encrypt(email, dynamicKey).toString();
+      sessionKeys[sessionID] = { dynamicKey, encryptedEmail: encryptedMail };
 
-    return res.status(200).json({
-      ok: true,
-      message: "Login successful",
-      userEmail: { email: user.email },
-    });
+      return res.status(200).json({
+        ok: true,
+        message: "Login successful",
+        // userEmail: { email: user.email },
+        userEmail: { sessionID },
+      });
+    }
   } catch (err) {
     console.error(err);
     res.status(500).json({ ok: false, message: "Server error" });
@@ -64,15 +74,34 @@ export const doLogin = async (req, res) => {
 
 export const profile = async (req, res) => {
   try {
-    const { email } = req.params; // localhost/profile/:email
+    const { sessionID } = req.params; // localhost/profile/:email
+    if (!sessionKeys[sessionID]) {
+      return res
+        .status(404)
+        .json({ ok: false, message: "session Expierd" })
+        .toString(CryptoJS.enc.Utf8);
+    }
+
+    if (!decryptedMail) {
+      return res
+        .status(400)
+        .json({ ok: false, message: "Email decryption failed" });
+    }
+
+    const { dynamicKey, encryptedEmail } = sessionKeys[sessionID];
+    const decryptedMail = crypto.AES.decrypt(
+      encryptedEmail,
+      dynamicKey
+    ).toString(CryptoJS.enc.Utf8);
+
     //email checking from db
-    const userEmail = await userModel.findOne({ email });
+    const userEmail = await userModel.findOne({ decryptedMail });
     if (!userEmail) {
       return res
         .status(400)
         .json({ ok: false, message: "userEmail not found" });
     } else {
-      return res.json(userEmail);
+      return res.json({ ok: true, email: userEmail });
     }
   } catch (err) {
     console.error(err);
--- server/routes/userRouter.route.js
+++ server/routes/userRouter.route.js
@@ -1,11 +1,14 @@
 import express from "express";
 const router = express.Router();
 
-import { doRegister, doLogin } from "../controller/userData.controller.js";
+import {
+  doRegister,
+  doLogin,
+  profile,
+} from "../controller/userData.controller.js";
 
 router.post("/doRegister", doRegister);
 router.post("/doLogin", doLogin);
+router.get("/profile/:sessionID", profile);
 
 export default router;
-
-
